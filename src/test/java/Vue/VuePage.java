c8cb0d01344a7036590487b6db3b2412b22418d4
Cheryl Headen <cherylheaden@MASTUDENT-Cheryl-Headen-FVFLF2561WFY.local>
2025-05-20 14:13:33 +0100

    imports added

--- src/test/java/Vue/VuePage.java
+++ src/test/java/Vue/VuePage.java
@@ -1,4 +1,10 @@
 package Vue;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import org.openqa.selenium.OutputType;
+import org.apache.commons.io.FileUtils;
 
 public class VuePage {
 
