36c98c9eca888be7eb5f5e1992978d7a85d9f7e6
Cheryl Headen <cherylheaden@MASTUDENT-Cheryl-Headen-FVFLF2561WFY.local>
2025-05-21 12:19:12 +0100

    First method and tests

--- src/test/java/ReactPage.java
+++ src/test/java/ReactPage.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
 public class ReactPage {
@@ -17,5 +18,14 @@ public class ReactPage {
         driver.get("https://todomvc.com/");
         js.executeScript("window.scrollTo(0,411)");
         driver.findElement(By.cssSelector(".js-app-list:nth-child(1) li:nth-child(1) .link")).click();
+    }
+
+    //method for input...(empty string, special characters, single character input, 2,3,4)
+    public void AddItem(String item) {
+        driver.findElement(By.id("todo-input")).click();
+        driver.findElement(By.id("todo-input")).sendKeys(item);
+        driver.findElement(By.id("todo-input")).sendKeys(Keys.ENTER);
+
+
     }
 }
--- src/test/java/ReactTest.java
+++ src/test/java/ReactTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,11 +28,95 @@ public class ReactTest {
     // TC-001: Add a new todo item with valid text.
     // Verify the item appears in the todo list.
     @Test
-    public void testAddValidTodo() {
+
+    public void test1AddValidTodo() {
         ReactPage reactPage = new ReactPage(driver);
         reactPage.navigate();
-        assertEquals("React", driver.findElement(By.xpath("/html/body/aside/header/h3")).getText());
+        reactPage.AddItem("Buy Milk");
+        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
+        boolean found = false;
+        for (WebElement label : labels) {
+            if (label.getText().equals("Buy Milk")) {
+
+                found = true;
+                break;
+
+            }
+
+
+        }
+        assertTrue(found);
+
+
     }
+    // TC-002: Reject empty input.
+    // Verify that submitting an empty input does not add a todo item.
+    @Test
+
+    public void test2RejectEmpty() {
+        ReactPage reactPage = new ReactPage(driver);
+        reactPage.navigate();
+        reactPage.AddItem("");
+        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
+        boolean found = false;
+        for (WebElement label : labels) {
+            if (label.getText().isEmpty()) {
+
+                found = true;
+                break;
+
+            }
+
+
+        }
+        assertFalse(found);
+    }
+    // TC-003: Accept special characters.
+    // Verify that a todo with special characters (e.g., @, #, $, %) can be added.
+    @Test
+
+    public void test3SpecialChars() {
+        ReactPage reactPage = new ReactPage(driver);
+        reactPage.navigate();
+        reactPage.AddItem("!!@£$");
+        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
+        boolean found = false;
+        for (WebElement label : labels) {
+            if (label.getText().equals("!!@£$")) {
+
+                found = true;
+                break;
+
+            }
+
+
+        }
+        assertTrue(found);
+    }
+
+    // TC-004: Accept single-character input.
+    // Verify that a todo item with a single character is accepted and displayed.
+    @Test
+
+    public void test4SingleChar() {
+        ReactPage reactPage = new ReactPage(driver);
+        reactPage.navigate();
+        reactPage.AddItem("C");
+        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
+        boolean found = false;
+        for (WebElement label : labels) {
+            if (label.getText().equals("C")) {
+
+                found = true;
+                break;
+
+            }
+
+
+        }
+        assertFalse(found);
+    }
+}
 
 
     // TC-002: Reject empty input.
@@ -53,5 +140,5 @@ public class ReactTest {
     // TC-018: Verify framework isolation.
     // Ensure that the data created or manipulated in the React version is not shared with or affecting any other framework version (e.g., Vue).
 
-}
+
 
