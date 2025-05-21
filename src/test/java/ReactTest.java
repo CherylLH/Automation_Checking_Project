8785d2c5889ebb294e7ec5184e3bac6016d89e8f
JosephGaughan <joe.s.gaughan@gmail.com>
2025-05-21 16:48:11 +0100

    Tidied up tests (7 passing)

--- src/test/java/ReactTest.java
+++ src/test/java/ReactTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -31,20 +30,22 @@ public class ReactTest {
 
     public void test1AddValidTodo() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "Buy milk";
         reactPage.navigate();
-        reactPage.AddItem("Buy Milk");
-        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
-        boolean found = false;
-        for (WebElement label : labels) {
-            if (label.getText().equals("Buy Milk")) {
-
-                found = true;
-                break;
-
-            }
-
-
-        }
+        reactPage.AddItem(item);
+//        List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
+//        boolean found = false;
+//        for (WebElement label : labels) {
+//            if (label.getText().equals("Buy Milk")) {
+//
+//                found = true;
+//                break;
+//
+//            }
+//
+//
+//        }
+        boolean found = reactPage.checkAdded(item);
         assertTrue(found);
 
 
@@ -52,44 +53,36 @@ public class ReactTest {
     // TC-002: Reject empty input.
     // Verify that submitting an empty input does not add a todo item.
     @Test
-
     public void test2RejectEmpty() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "";
         reactPage.navigate();
-        reactPage.AddItem("");
+        reactPage.AddItem(item);
         List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
         boolean found = false;
         for (WebElement label : labels) {
             if (label.getText().isEmpty()) {
-
                 found = true;
                 break;
-
             }
-
-
         }
         assertFalse(found);
     }
     // TC-003: Accept special characters.
     // Verify that a todo with special characters (e.g., @, #, $, %) can be added.
     @Test
-
     public void test3SpecialChars() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "!!@£$";
         reactPage.navigate();
-        reactPage.AddItem("!!@£$");
+        reactPage.AddItem(item);
         List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
         boolean found = false;
         for (WebElement label : labels) {
-            if (label.getText().equals("!!@£$")) {
-
+            if (label.getText().equals(item)) {
                 found = true;
                 break;
-
             }
-
-
         }
         assertTrue(found);
     }
@@ -97,22 +90,18 @@ public class ReactTest {
     // TC-004: Accept single-character input.
     // Verify that a todo item with a single character is accepted and displayed.
     @Test
-
     public void test4SingleChar() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "C";
         reactPage.navigate();
-        reactPage.AddItem("C");
+        reactPage.AddItem(item);
         List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
         boolean found = false;
         for (WebElement label : labels) {
-            if (label.getText().equals("C")) {
-
+            if (label.getText().equals(item)) {
                 found = true;
                 break;
-
             }
-
-
         }
         assertFalse(found);
     }
@@ -120,53 +109,48 @@ public class ReactTest {
     @Test
     public void test5MarkComplete() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "Buy milk";
         reactPage.navigate();
-        String addedItem = "Buy milk";
-        reactPage.AddItem(addedItem);
-        reactPage.toggleComplete(addedItem);
-        boolean found = reactPage.isItemMarkedCompleted("Buy milk");
+        reactPage.AddItem(item);
+        reactPage.toggleComplete(item);
+        boolean found = reactPage.isItemMarkedCompleted(item);
         assertTrue(found);
     }
 
     @Test
     public void test6MarkIncomplete() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "Buy milk";
         reactPage.navigate();
-        String addedItem = "Buy milk";
-        reactPage.AddItem(addedItem);
-        reactPage.toggleComplete(addedItem);
-        boolean found = reactPage.isItemMarkedCompleted("Buy milk");
+        reactPage.AddItem(item);
+        reactPage.toggleComplete(item);
+        boolean found = reactPage.isItemMarkedCompleted(item);
         assertTrue(found);
-        reactPage.toggleComplete(addedItem);
-        found = reactPage.isItemMarkedCompleted("Buy milk");
+        reactPage.toggleComplete(item);
+        found = reactPage.isItemMarkedCompleted(item);
         assertFalse(found);
     }
 
     @Test
     public void test7ClearCompleted() {
         ReactPage reactPage = new ReactPage(driver);
+        String item = "Buy milk";
         reactPage.navigate();
-        String addedItem = "Buy milk";
-        reactPage.AddItem(addedItem);
-        reactPage.toggleComplete(addedItem);
-        boolean found = reactPage.isItemMarkedCompleted("Buy milk");
+        reactPage.AddItem(item);
+        reactPage.toggleComplete(item);
+        boolean found = reactPage.isItemMarkedCompleted(item);
         assertTrue(found);
-        //reactPage.AddItem("Buy bread");
+        //reactPage.AddItem("Buy bread"); // In case the absence of items made the element undiscoverable
         reactPage.clearCompleted();
         List<WebElement> labels = driver.findElements(By.cssSelector("label[data-testid='todo-item-label']"));
         found = false;
         for (WebElement label : labels) {
-            if (label.getText().equals("Buy Milk")) {
-
+            if (label.getText().equals(item)) {
                 found = true;
                 break;
-
             }
-
-
         }
         assertFalse(found);
-
     }
 }
 
@@ -191,6 +175,3 @@ public class ReactTest {
 
     // TC-018: Verify framework isolation.
     // Ensure that the data created or manipulated in the React version is not shared with or affecting any other framework version (e.g., Vue).
-
-
-
